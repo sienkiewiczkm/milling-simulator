7473c133aec7464ae97a5b0403206b06704c1653
Kamil Sienkiewicz <sienkiewiczkm@gmail.com>
2016-11-13 16:56:02 +0100

    Segment-segment intersections

---
+++ src/mill-gui/GeometricIntersections.hpp
@@ -0,0 +1,98 @@
+#pragma once
+#include "glm/glm.hpp"
+#include <algorithm>
+
+namespace fw
+{
+
+enum class GeometricIntersectionKind
+{
+    Unknown,
+    None,
+    Single,
+    Multiple,
+    Infinite
+};
+
+template <typename TPrecision>
+struct GeometricIntersectionResult
+{
+    GeometricIntersectionResult():
+        kind{GeometricIntersectionKind::Unknown}
+    {
+    }
+
+    GeometricIntersectionResult(const GeometricIntersectionKind &kind):
+        kind{kind}
+    {
+    }
+
+    GeometricIntersectionKind kind;
+    TPrecision singleIntersectionParameter;
+};
+
+template <typename TVector2D, typename TPrecision>
+GeometricIntersectionResult<TPrecision> intersectSegments(
+    const TVector2D& aStart,
+    const TVector2D& aEnd,
+    const TVector2D& bStart,
+    const TVector2D& bEnd
+)
+{
+    GeometricIntersectionResult<TPrecision> result;
+    // stackoverflow: how-do-you-detect-where-two-line-segments-intersect
+    // http://stackoverflow.com/a/565282
+
+    auto r = aEnd - aStart;
+    auto s = bEnd - bStart;
+    auto qp = bStart - aStart;
+
+    // cross products
+    auto rxs = r.x * s.y - r.y * s.x;
+    auto qpxr = qp.x * r.y - qp.y * r.x;
+    auto qpxs = qp.x * s.y - qp.y * s.x;
+
+    const TPrecision eps = static_cast<TPrecision>(10e-6);
+    if (std::abs(rxs) <= eps && std::abs(qpxr) <= eps)
+    {
+        auto t0 = glm::dot(bStart - aStart, r) / glm::dot(r, r);
+        auto t1 = t0 + glm::dot(s, r) / glm::dot(r, r);
+        if (t0 > t1) { std::swap(t0, t1); }
+        t0 = std::max(static_cast<TPrecision>(0), t0);
+        t1 = std::min(static_cast<TPrecision>(1), t1);
+        if (0 <= t0 && t0 <= 1 && 0 <= t1 && t1 <= 1)
+        {
+            // todo: return t0 and t1 somehow
+            result.kind = GeometricIntersectionKind::Infinite;
+        }
+        else
+        {
+            result.kind = GeometricIntersectionKind::None;
+        }
+
+        return result;
+    }
+
+    if (std::abs(rxs) <= eps && std::abs(qpxr) > eps)
+    {
+        result.kind = GeometricIntersectionKind::None;
+        return result;
+    }
+
+    if (std::abs(rxs) > eps)
+    {
+        auto t = qpxs / rxs;
+        auto u = qpxr / rxs;
+        if (0 <= t && t <= 1 && 0 <= u && u <= 1)
+        {
+            result.singleIntersectionParameter = t;
+            result.kind = GeometricIntersectionKind::Single;
+            return result;
+        }
+    }
+
+    result.kind = GeometricIntersectionKind::None;
+    return result;
+}
+
+}
